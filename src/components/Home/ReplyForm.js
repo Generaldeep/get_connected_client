46711c99155be0f8c4e586d1db3294acdf5e5169
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-08-23 12:21:02 -0700

    reply icon is clickabl

--- src/components/Home/ReplyForm.js
+++ src/components/Home/ReplyForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { Button, Form, Input, Icon } from 'semantic-ui-react';
+import { Button, Form, Input, Icon, Modal, TextArea } from 'semantic-ui-react';
 import {addComment} from '../../actions';
 import './home.css';
 
@@ -9,7 +9,8 @@ class Comments extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: ''
+      input: '',
+      modalOpen: false
     }
   }
   updateComment = (event) => {
@@ -17,20 +18,45 @@ class Comments extends Component {
       input: event.target.value
     })
   }
+
+  handleOpen = (e) => this.setState({
+  modalOpen: true,
+  })
+
+  handleClose = (e) => this.setState({
+    modalOpen: false,
+  })
+
     render() {
      const { userData, addComment, postKey, postIndex } = this.props;
         return (
-        //  <Form id='commentForm'>
-        //      <Input placeholder="Add Reply" name="comment" onChange={this.updateComment} value={this.state.input} type="text"/>
-        //      <br></br>
-        //      <br></br>
-        //    <Button color='blue' className="submit" type="submit" onClick={(e) => {
-        //     e.preventDefault();
-        //     addComment(userData, this.state.input, postKey, postIndex)
-        //     this.setState({input: ''})
-        //     }}>Submit</Button>
-        //  </Form>
-        <Icon>Reply</Icon>
+          <div>
+            <Icon  type="submit" name='reply' size='large' link='true' onClick={this.handleOpen} />
+            <Modal open={this.state.modalOpen}>
+              <Modal.Header>Add Reply</Modal.Header>
+              <Modal.Content>
+                 <Form className="links ui form">
+                      <Modal.Content>
+                        <TextArea rows="2" value={this.state.input} className="editcomment"  onChange={this.updateComment} />
+                     </Modal.Content>
+
+                      <Button color='blue' className="submit" type="submit" onClick={(e) => {
+                       e.preventDefault();
+                       addComment(userData, this.state.input, postKey, postIndex)
+                       this.setState({input: ''})
+                       this.handleClose()
+                       }}>Submit</Button>
+
+                    <div className="cancel">
+                      <Button color="red" type="cancel" onClick={(e) =>{
+                        e.preventDefault();
+                        this.handleClose()
+                      }}>Cancel</Button>
+                    </div>
+                </Form>
+               </Modal.Content>
+             </Modal>
+           </div>
         )
     }
 }
