45176b1577f2ff46bb1410b2c5ea008a3cf5626e
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-08-30 15:18:40 -0700

    Removed borders from jobs/articles

--- src/components/GoogleAuth/LearnMore.js
+++ src/components/GoogleAuth/LearnMore.js
@@ -7,7 +7,7 @@ const LearnMore = () => (
     <h1>Get_Connected</h1>
     <div className="description">
       <p className="welcomeparagraph">
-        Welcome to Get_Connected. The best way to maintain communication with your Galvanize friends, share your skills & projects, see job postings and event get tech news.
+        Welcome to Get_Connected. The best way to maintain communication with your Galvanize friends, share your skills & projects, see job postings and even get tech news.
        </p>
 
       {/* <p>
