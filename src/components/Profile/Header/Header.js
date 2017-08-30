449647bfdc9c50af580bc7e7c36ca275427d15de
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-08-30 16:29:57 -0700

    Changed profile header to use semantic header, color to blue

--- src/components/Profile/Header/Header.js
+++ src/components/Profile/Header/Header.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import semantic from 'semantic-ui-react';
 import AddCohort from '../Cohort/AddCohort';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
 import './profile.css';
 
 const ProfileHeader = ({ userData }) => (
   <Grid className="userPro" stackable>
-    <Grid.Row>
-      <div className="userName">
-        <h1>Weclome To {userData.name}'s Profile</h1>
+    <Grid.Column className="userName">
+        <Header size='tiny' color='blue'>Weclome To {userData.name}'s Profile</Header>
         <Image className="ui circular image imgPro" src={userData.photo} alt={userData.name} />
-      </div>
-    </Grid.Row>
+    </Grid.Column>
   </Grid>
     );
 
