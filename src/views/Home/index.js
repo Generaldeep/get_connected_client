4e55a4d892da86fc573e1598be65616d3e2cf9ea
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-08-30 09:41:29 -0700

    Semantic grid/columns are responsive to device size

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,6 +1,6 @@
 import React,{ Component } from 'react';
 import { connect } from 'react-redux';
-import { Grid } from 'semantic-ui-react'
+import { Grid, Header } from 'semantic-ui-react'
 import GetJobs from '../../components/Home/GetJobs';
 import PostForm from '../../components/Home/PostForm';
 import Feed from '../../components/Home/Feed';
@@ -10,34 +10,52 @@ import './home.css'
 class Home extends Component {
   render() {
     return(
-      <Grid id="home" stackable columns={1} >
-        <Grid.Row columns={3}>
-          <Grid.Column>
-            <GetJobs />
-          </Grid.Column>
-          <Grid.Column>
-            <Feed />
-          </Grid.Column>
-          <Grid.Column>
-            <Articles />
-          </Grid.Column>
-        </Grid.Row>
-        </Grid>
-      {/* <Grid id="home" stackable columns={1} >
-        <Grid.Row columns={3}>
-          <Grid.Column>
-            <GetJobs />
-          </Grid.Column>
-          <Grid.Column>
-            <Feed />
-          </Grid.Column>
-          <Grid.Column>
-            <Articles />
-          </Grid.Column>
-        </Grid.Row>
-        </Grid> */}
+      <Grid>
+        <Grid.Column only='computer' computer={5}>
+          <Header>Articles</Header>
+          <Articles/>
+        </Grid.Column>
+        <Grid.Column mobile={16} tablet={8} computer={5}>
+          <Feed/>
+        </Grid.Column>
+        <Grid.Column mobile={16} tablet={8} computer={5}>
+          <Header>Bay Area Jobs</Header>
+          <GetJobs/>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
 
 export default Home;
+
+{/* <Grid>
+  <Grid.Column only='computer' computer={5}>
+    <Header>Articles</Header>
+  </Grid.Column>
+  <Grid.Column mobile={16} tablet={8} computer={5}>
+    <Feed/>
+  </Grid.Column>
+  <Grid.Column mobile={16} tablet={8} computer={5}>
+    <GetJobs/>
+  </Grid.Column>
+  <Grid.Column only='mobile tablet' mobile={16} tablet={16}>
+    <Header>Articles</Header>
+  </Grid.Column>
+</Grid> */}
+
+{/* <Grid id="home" stackable columns={3}>
+  <Grid.Row>
+    <Grid.Column>
+      <Header>Articles</Header>
+      <Articles/>
+    </Grid.Column>
+    <Grid.Column>
+      <Feed/>
+    </Grid.Column>
+    <Grid.Column>
+      <Header>Jobs</Header>
+      <GetJobs/>
+    </Grid.Column>
+  </Grid.Row>
+</Grid> */}
