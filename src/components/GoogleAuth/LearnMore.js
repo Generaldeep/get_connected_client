dad8005490e5058bccd105d2ab2375745a94b607
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-09-07 13:48:14 -0700

    Removed contact me paragraph from login page

--- src/components/GoogleAuth/LearnMore.js
+++ src/components/GoogleAuth/LearnMore.js
@@ -9,14 +9,6 @@ const LearnMore = () => (
       <p className="welcomeparagraph">
         Welcome to Get_Connected. The best way to maintain communication with your Galvanize friends, share your skills & projects, see job postings and even get tech news.
        </p>
-
-      {/* <p>
-        Although Get_Connected requires users to sign in with Google, you can easily delete your account once you are logged in. Go to profile, and click delete profile and you are all set.
-       </p> */}
-
-      <p>
-        This website is constantly improving. New features are being added weekly, while other features are being refactored. If you have any questions, or suggestions, email me at Gurdipsingh1990@gmail.com
-       </p>
     </div>
   </div>
     );
